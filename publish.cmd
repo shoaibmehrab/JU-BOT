cc4e92a411cecc30d5c20442d495ae66272b6560
Shoaib Ahmed <shoaibmehrab011@gmail.com>
2018-09-02 10:16:24 +0600

    Add project files.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=jumamabot-Web-Deploy.pubxml -p:Password=EGE1aNwKRs0GPqLbtztYZ1ah4HiTBTs1iACub5vYoYYni4J3uQvTQs2LKmbW
+
